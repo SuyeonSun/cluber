1642d62159a8a461e651dea9029798da73d0b255
SuyeonSun <suninisy@gmail.com>
2021-10-12 21:36:42 +0900

    코드 수정 전 커밋

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport'); // 
 
 const Login = require('../controllers/login/Login');
 router.get('/', Login.index);
